4421007b2da1eeb485dbee1d6f26084fe1d48e69
ShubhankarBhavsar83 <shubhankarbhavsar83@gmail.com>
2026-02-27 18:33:28 +0530

    Updated MUI version

--- next.config.ts
+++ next.config.ts
@@ -3,10 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   experimental: {
     optimizePackageImports: ["@mui/material", "@mui/x-data-grid"],
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-} as any;
+  }
+};
 
 export default nextConfig;
